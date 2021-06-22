42c27c29b24f4ea780630019f31c7da7db91d640
AwsRadwan <awsradwan3@gmail.com>
2021-06-22 18:26:48 +0300

    start with spring models

---
+++ Spring_MVC/edit/src/test/java/com/aws/edit/EditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.edit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
